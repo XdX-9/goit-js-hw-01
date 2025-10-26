39e3151155abf0d7e6fa1f20f0878c0ba4723530
XdX <prostoakkaunt823@gmail.com>
2025-10-26 17:32:45 +0200

    js hw1

---
+++ js/task-3.js
@@ -0,0 +1,9 @@
+function getElementWidth(content, padding, border) {
+  const elementWidht =
+    parseInt(content) + parseInt(padding) * 2 + parseInt(border) * 2;
+  console.log(elementWidht);
+}
+
+console.log(getElementWidth('50px', '8px', '4px')); // 74
+console.log(getElementWidth('60px', '12px', '8.5px')); // 101
+console.log(getElementWidth('200px', '0px', '0px')); // 200
